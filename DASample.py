3a82fdc8190a1d6072332f7769f939b117a3bae8
EdwinDavids <148939800+EdwinDavids@users.noreply.github.com>
2024-06-05 11:57:33 +0530

    Update DASample.py

--- DASample.py
+++ DASample.py
@@ -10,7 +10,7 @@ spark = SparkSession.builder\
     .getOrCreate()
 
 # Read the text file into a DataFrame
-text_df = spark.read.text("/home/edwindavid/PycharmProjects/newhive/Sparkprojects/Intership/sampletext.txt")
+text_df = spark.read.text("sampletext.txt")
 
 # Split each line into words and count the occurrences of each word
 word_counts = text_df.select(explode(split(text_df.value, " ")).alias("word")) \
